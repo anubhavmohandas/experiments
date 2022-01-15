e137bc426ea23594a291cefcf7fba49f36754aa0
anubhavmohandas <anubhav.manav147@gmail.com>
2022-01-15 22:18:33 +0530

    experiments

---
+++ Coupon/coupon.js
@@ -0,0 +1,7 @@
+function coupon(){
+    document.getElementById('coupon-container').style.visibility = "visible";
+}
+
+function closeDiv(){
+    document.getElementById('coupon-container').style.visibility = "hidden";
+}
